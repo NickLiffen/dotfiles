f706850b98604cbdf62a5a0fcd362df5026e9fcc
Nick Liffen <nickliffen@gmail.com>
2021-11-05 21:30:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-setupsolarized dircolors.ansi-dark
+ln -s $(pwd)/aliases.zsh $HOME/.aliases.zsh
+ln -s $(pwd)/gitconfig $HOME/.gitconfig
